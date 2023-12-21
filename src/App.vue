0463720d95ac421f0ec8fefef8bf01fb1894d984
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-21 15:53:55 +0700

    header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="py-4 fixed top-0 left-0 w-full">
+    <div class="max-w-4xl mx-auto flex items-center justify-between">
+      <a href="" class="text-white text-base font-bold">Home</a>
+      <ul class="flex items-center gap-x-8">
+        <li><a href="" class="text-white text-sm">About</a></li>
+        <li><a href="" class="text-white text-sm">Cabang</a></li>
+        <li><a href="" class="text-white text-sm">Masuk Sebagai Cabang</a></li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="bg-sky-600 header text-white py-40">
+    <div class="max-w-4xl mx-auto">
+      <div class="w-3/4 mx-auto text-center">
+        <h1 class="text-4xl font-bold tracking-tight mb-2">
+          Komunitas Air Hujan
+        </h1>
+        <p class="text-lg mb-4">
+          Bersama memanfaatkan air hujan menjadi sesuatu yang lebih bermanfaat
+        </p>
+        <input
+          type="search"
+          class="mb-2 px-3.5 py-2.5 w-full border-0 rounded-xl focus:outline-0 focus:ring-0 text-gray-700 text-sm leading-0"
+          placeholder="Cari Cabang Komunitas"
+        />
+        <a href="" class="text-xs text-white">Lihat Semua Cabang</a>
+      </div>
+    </div>
+  </header>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
