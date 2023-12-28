3e136a864d7b7129840bba1723b7a1327f08ab0d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-28 09:29:20 +0700

    asdkas

--- src/App.vue
+++ src/App.vue
@@ -28,14 +28,48 @@
     </div>
   </header>
   <div class="max-w-4xl mx-auto py-20 space-y-6">
-    <div class="border-b border-gray-200 pb-6">
+    <div
+      class="border-b border-gray-200 pb-6 flex items-center justify-between"
+    >
       <h3 class="text-base font-semibold leading-6 text-gray-900">
-        Job Postings
+        Top Komunitas
       </h3>
+      <a
+        href=""
+        class="px-3 py-1.5 border border-gray-200 bg-white text-gray-900 text-sm rounded-md"
+        >Lihat Semua</a
+      >
     </div>
     <div class="grid grid-cols-3 gap-6">
       <div v-for="x in 6" :key="x" class="border rounded-xl">
         <div class="bg-gray-100 rounded-t-xl h-[150px]"></div>
+        <div class="rounded-b-xl p-4">
+          <p class="text-xs text-gray-500 mb-1">Arsenal</p>
+          <a href="" class="font-bold text-base block mb-2">Bad Boy</a>
+          <p class="text-sm text-gray-700 leading-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            voluptatibus minima repellendus.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="max-w-4xl mx-auto py-20 space-y-6">
+    <div
+      class="border-b border-gray-200 pb-6 flex items-center justify-between"
+    >
+      <h3 class="text-base font-semibold leading-6 text-gray-900">
+        Aktifitas Terbaru
+      </h3>
+      <a
+        href=""
+        class="px-3 py-1.5 border border-gray-200 bg-white text-gray-900 text-sm rounded-md"
+        >Lihat Semua</a
+      >
+    </div>
+    <div class="grid grid-cols-1 gap-6">
+      <div v-for="x in 5" :key="x" class="border rounded-xl flex">
+        <div class="bg-gray-100 rounded-l-xl w-[100px] h-full"></div>
         <div class="rounded-b-xl p-4">
           <p class="text-xs text-gray-500 mb-1">Arsenal</p>
           <a href="" class="font-bold text-base block mb-2">Bad Boy</a>
