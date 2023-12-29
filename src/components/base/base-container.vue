b6e3b9767339d4c3337dec95a9b8ba74d316ba5d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-29 21:33:46 +0700

    container

---
+++ src/components/base/base-container.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="max-w-4xl mx-auto px-4 lg:px-0">
+    <slot />
+  </div>
+</template>
--- src/components/base/base-navbar.vue
+++ src/components/base/base-navbar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import BaseContainer from './base-container.vue';
+
 defineProps({
   navs: {
     type: Array,
@@ -9,13 +11,15 @@ defineProps({
 
 <template>
   <div class="py-4 fixed top-0 left-0 w-full">
-    <div class="max-w-4xl mx-auto flex items-center justify-between">
-      <a href="" class="text-white text-base font-bold">Home</a>
-      <ul class="flex items-center gap-x-8">
-        <li v-for="nav in navs" :key="nav.id">
-          <a href="" class="text-white text-sm">{{ nav.name }}</a>
-        </li>
-      </ul>
-    </div>
+    <base-container>
+      <div class="flex items-center justify-between">
+        <a href="" class="text-white text-base font-bold">Home</a>
+        <ul class="flex items-center gap-x-8">
+          <li v-for="nav in navs" :key="nav.id">
+            <a href="" class="text-white text-sm">{{ nav.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </base-container>
   </div>
 </template>
