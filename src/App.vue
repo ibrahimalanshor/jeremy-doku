c042c91864809c48c1f57d008c330dbde67c30e1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-03 19:02:40 +0700

    event vertical card list

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,10 @@ import LandingNavbar from 'src/components/landing/landing-navbar.vue';
 import LandingHeader from 'src/components/landing/landing-header.vue';
 import BaseHeading from 'src/components/base/base-heading.vue';
 import BaseButton from 'src/components/base/base-button.vue';
-import BaseCard from 'src/components/base/base-card.vue';
-import BaseHorizontalCard from 'src/components/base/base-horizontal-card.vue';
 import BaseSectionHeading from 'src//components/base/base-section-heading.vue';
 import BaseContainer from 'src/components/base/base-container.vue';
-import CommunityGridList from 'src/features/community/components/community-grid-list.vue';
+import CommunityCardList from 'src/features/community/components/community-card-list.vue';
+import EventHorizontalCardList from 'src/features/event/components/event-horizontal-card-list.vue';
 </script>
 
 <template>
@@ -22,7 +21,7 @@ import CommunityGridList from 'src/features/community/components/community-grid-
         <base-button size="md" color="white-bordered">Lihat Semua</base-button>
       </template>
     </base-section-heading>
-    <community-grid-list />
+    <community-card-list />
   </base-container>
   <base-container class="py-20 space-y-6">
     <base-section-heading>
@@ -33,15 +32,7 @@ import CommunityGridList from 'src/features/community/components/community-grid-
         <base-button size="md" color="white-bordered">Lihat Semua</base-button>
       </template>
     </base-section-heading>
-    <div class="grid grid-cols-1 gap-6">
-      <base-horizontal-card
-        v-for="x in 5"
-        :key="x"
-        subtitle="Sleman"
-        title="Banyu Bening"
-        description="Temukan aneka produk Pedal Wah Vox terlengkap yang dapat digunakan untuk berbagai macam brand"
-      />
-    </div>
+    <event-horizontal-card-list />
   </base-container>
   <div class="bg-neutral-900 text-white py-4">
     <base-container class="text-center text-sm font-bold">
---
+++ src/features/event/components/event-horizontal-card-list.vue
@@ -0,0 +1,15 @@
+<script setup>
+import BaseHorizontalCard from 'src/components/base/base-horizontal-card.vue';
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-6">
+    <base-horizontal-card
+      v-for="x in 5"
+      :key="x"
+      subtitle="Sleman"
+      title="Banyu Bening"
+      description="Temukan aneka produk Pedal Wah Vox terlengkap yang dapat digunakan untuk berbagai macam brand"
+    />
+  </div>
+</template>
