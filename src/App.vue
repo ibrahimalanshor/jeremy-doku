ac6f849c43e9bc3881d113f9e2b9080e6cce48ec
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-03 18:56:09 +0700

    base section heading

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@ import BaseHeading from 'src/components/base/base-heading.vue';
 import BaseButton from 'src/components/base/base-button.vue';
 import BaseCard from 'src/components/base/base-card.vue';
 import BaseHorizontalCard from 'src/components/base/base-horizontal-card.vue';
+import BaseSectionHeading from 'src//components/base/base-section-heading.vue';
+import BaseContainer from 'src/components/base/base-container.vue';
 </script>
 
 <template>
   <landing-navbar />
   <landing-header />
-  <div class="max-w-4xl mx-auto py-20 space-y-6">
-    <div
-      class="border-b border-gray-200 pb-6 flex items-center justify-between"
-    >
+  <base-container class="py-20 space-y-6">
+    <base-section-heading>
       <base-heading size="md" weight="semibold">Top Komunitas</base-heading>
       <base-button size="md" color="white-bordered">Lihat Semua</base-button>
-    </div>
+    </base-section-heading>
     <div class="grid grid-cols-3 gap-6">
       <base-card
         v-for="x in 6"
@@ -26,14 +26,12 @@ import BaseHorizontalCard from 'src/components/base/base-horizontal-card.vue';
         description="Temukan aneka produk Pedal Wah Vox terlengkap yang dapat digunakan untuk berbagai macam brand"
       />
     </div>
-  </div>
-  <div class="max-w-4xl mx-auto py-20 space-y-6">
-    <div
-      class="border-b border-gray-200 pb-6 flex items-center justify-between"
-    >
+  </base-container>
+  <base-container class="py-20 space-y-6">
+    <base-section-heading>
       <base-heading size="md" weight="semibold">Events Terbaru</base-heading>
       <base-button size="md" color="white-bordered">Lihat Semua</base-button>
-    </div>
+    </base-section-heading>
     <div class="grid grid-cols-1 gap-6">
       <base-horizontal-card
         v-for="x in 5"
@@ -43,10 +41,10 @@ import BaseHorizontalCard from 'src/components/base/base-horizontal-card.vue';
         description="Temukan aneka produk Pedal Wah Vox terlengkap yang dapat digunakan untuk berbagai macam brand"
       />
     </div>
-  </div>
+  </base-container>
   <div class="bg-neutral-900 text-white py-4">
-    <div class="max-w-4xl mx-auto text-center text-sm font-bold">
+    <base-container class="text-center text-sm font-bold">
       Sekolah Hujan
-    </div>
+    </base-container>
   </div>
 </template>
---
+++ src/components/base/base-section-heading.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="border-b border-gray-200 pb-6 flex items-center justify-between">
+    <slot name="start" />
+    <slot name="end" />
+  </div>
+</template>
